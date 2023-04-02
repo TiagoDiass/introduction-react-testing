60102482887a6c953b63d2712c8edd93a261dbc8
TiagoDiass <tiago.costadiasss@gmail.com>
2023-04-01 23:16:16 -0300

    chore: add boilerplate code for Calculator.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
 import Counter from './components/Counter/Counter';
+import Calculator from './components/Calculator/Calculator';
+import { useState } from 'react';
 
 function App() {
+  const [currentComponent, setCurrentComponent] = useState('counter');
+
   return (
     <div className='App'>
       <h1>Tests introduction</h1>
+      <div className='components'>
+        <button onClick={() => setCurrentComponent('counter')}>Counter</button>
+        <button onClick={() => setCurrentComponent('calculator')}>
+          Calculator
+        </button>
+      </div>
 
-      <Counter />
+      {currentComponent === 'counter' && <Counter />}
+      {currentComponent === 'calculator' && <Calculator />}
     </div>
   );
 }
---
+++ src/components/Calculator/Calculator.test.tsx
@@ -0,0 +1,3 @@
+import Calculator from './Calculator';
+
+describe('Component: Calculator', () => {});
