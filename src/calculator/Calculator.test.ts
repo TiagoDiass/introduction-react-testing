39c77060d29dc2236912589fb86753ec94b518a7
TiagoDiass <tiago.costadiasss@gmail.com>
2023-04-01 23:09:25 -0300

    test: finish tests of Calculator class

--- src/calculator/Calculator.test.ts
+++ src/calculator/Calculator.test.ts
@@ -5,7 +5,15 @@ describe('Calculator', () => {
     expect(Calculator.sum(3, 7)).toBe(10);
   });
 
+  it('should subtract correctly', () => {
+    expect(Calculator.subtract(10, 7)).toBe(3);
+  });
+
   it('should multiply correctly', () => {
     expect(Calculator.multiply(2, 10)).toBe(20);
+  });
+
+  it('should divide correctly', () => {
+    expect(Calculator.divide(10, 5)).toBe(2);
   });
 });
